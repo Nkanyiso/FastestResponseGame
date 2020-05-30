ab5244756bfcea2d169855d5fc70d9d8e2b3e619
Nkanyiso Hlela <hlela13@gmail.com>
2020-05-30 12:37:16 +0200

    android project create added navigation component

---
+++ app/src/main/java/com/madlula/fastestresponse/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.madlula.fastestresponse.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.madlula.fastestresponse.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
