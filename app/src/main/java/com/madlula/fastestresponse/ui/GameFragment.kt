597660e6d583ba90d1bb8a82307f0d8266c30fe6
Nkanyiso Hlela <hlela13@gmail.com>
2020-06-01 05:53:58 +0200

    added game screen shows arrows in choosen color

--- app/src/main/java/com/madlula/fastestresponse/ui/GameFragment.kt
+++ app/src/main/java/com/madlula/fastestresponse/ui/GameFragment.kt
@@ -1,16 +1,23 @@
 package com.madlula.fastestresponse.ui
 
-import android.media.Image
+import android.app.Application
+import android.content.Context
+import android.content.res.Resources
+import android.hardware.SensorManager
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.os.Handler
+import android.util.Log
+import android.view.*
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.madlula.fastestresponse.R
 import com.madlula.fastestresponse.databinding.FragmentGameBinding
 import com.madlula.fastestresponse.viewModel.BaseViewModel
 import com.madlula.fastestresponse.viewModel.Event
+
 
 /**
  * Game fragment where tilt is detected.
@@ -18,6 +25,12 @@ import com.madlula.fastestresponse.viewModel.Event
 class GameFragment : Fragment() {
     private lateinit var binding: FragmentGameBinding
     private lateinit var viewModel: BaseViewModel
+    var numberOfRounds = 10
+    var TAG = "Game fragment"
+
+    lateinit var mOrientationEventListener: OrientationEventListener
+    private var mDisplay: Display? = null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
@@ -28,13 +41,47 @@ class GameFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        Toast.makeText(requireContext(), getString(R.string.txt_game_begun), Toast.LENGTH_LONG).show()
         viewModel.getArrowDirection().observe(requireActivity(), Event.Observer {
             showArrow(it)
         })
-        viewModel.generateDirection()
+        viewModel.getIntervalTime().observe(requireActivity(), Event.Observer {
+            interval(it)
+        })
+        viewModel.generateInterval()
+        viewModel.score.observe(requireActivity(), Observer {
+            binding.txtScore.text = "" + it
+        })
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mOrientationEventListener = object : OrientationEventListener(requireContext(),
+                SensorManager.SENSOR_DELAY_NORMAL) {
+            override fun onOrientationChanged(orientation: Int) {
+                viewModel.detectedTilt(orientation)
+                // Toast.makeText(requireContext(), "tilted : " + orientation, Toast.LENGTH_LONG).show()
+
+            }
+        }
+
+        if (mOrientationEventListener.canDetectOrientation() === true) {
+            Log.v("DEBUG_TAG", "Can detect orientation")
+            mOrientationEventListener.enable()
+        } else {
+            Log.v("DEBUG_TAG", "Cannot detect orientation")
+            mOrientationEventListener.disable()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mOrientationEventListener.disable()
     }
 
     fun hideAllArrows() {
+        viewModel.arrowShown = false
         binding.arrowForward.visibility = View.GONE
         binding.arrowRight.visibility = View.GONE
         binding.arrowBack.visibility = View.GONE
@@ -44,6 +91,7 @@ class GameFragment : Fragment() {
 
     fun showArrow(direction: Int) {
         var arrow: ImageView
+        var colorList = (resources.getStringArray(R.array.color_list))
         when (direction) {
             1 -> arrow = binding.arrowForward
             2 -> arrow = binding.arrowRight
@@ -52,7 +100,37 @@ class GameFragment : Fragment() {
             else -> arrow = binding.arrowForward
         }
         arrow.visibility = View.VISIBLE
+        arrow.setColorFilter(requireActivity().getResources().getColor(viewModel.getColor()))
+        ///arrow.setColorFilter(resources.getIdentifier(colorList[viewModel.choosenColor.value!!].toString(),"colors",requireContext().packageName))
+        viewModel.arrowShown = true
+        viewModel.waitingToShowArrow = false
+        numberOfRounds--
+        if (numberOfRounds != 0) {
+            Handler().postDelayed({
+                viewModel.generateInterval()
+                //  Toast.makeText(requireContext(), "Round number : " + numberOfRounds, Toast.LENGTH_LONG).show()
+            }, 3000)//only show arrow for 2 seconds
+        } else {
+            //game finished
+            Handler().postDelayed({
+                hideAllArrows()
+                Toast.makeText(requireContext(), getString(R.string.txt_game_finished), Toast.LENGTH_LONG).show()
+            }, 3000)
+
+        }
 
     }
+
+    fun interval(delayTime: Long) {
+        hideAllArrows()
+        // Toast.makeText(requireContext(),getString(R.string.txt_game_finished),Toast.LENGTH_LONG).show()
+        viewModel.waitingToShowArrow = true
+        viewModel.isTilted = false
+        Handler().postDelayed({
+            viewModel.generateDirection()
+        }, delayTime)
+
+    }
+
 
 }
--- app/src/main/java/com/madlula/fastestresponse/viewModel/BaseViewModel.kt
+++ app/src/main/java/com/madlula/fastestresponse/viewModel/BaseViewModel.kt
@@ -3,22 +3,83 @@ package com.madlula.fastestresponse.viewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.madlula.fastestresponse.R
 
- class BaseViewModel : ViewModel(){
-     var choosenColor : MutableLiveData<Int> = MutableLiveData()
-     var score : MutableLiveData<Int> = MutableLiveData()
-     var appInitialized : MutableLiveData<Event<Boolean>> = MutableLiveData<Event<Boolean>>()
-     var tiltDirection : MutableLiveData<Event<Int>> = MutableLiveData<Event<Int>>()
+class BaseViewModel : ViewModel() {
+    var choosenColor: MutableLiveData<Int> = MutableLiveData()
+    var score: MutableLiveData<Int> = MutableLiveData<Int>()
+    var appInitialized: MutableLiveData<Event<Boolean>> = MutableLiveData<Event<Boolean>>()
+    var tiltDirection: MutableLiveData<Event<Int>> = MutableLiveData<Event<Int>>()
+    var intervalTime: MutableLiveData<Event<Long>> = MutableLiveData<Event<Long>>()
+    var arrowShown: Boolean = false
+    var waitingToShowArrow: Boolean = false
+    var isTilted = false;
 
 
-     fun isAppInitialized(): LiveData<Event<Boolean>> = appInitialized
-     fun init(){
-         appInitialized.value = Event(true)
-     }
-     fun getArrowDirection(): LiveData<Event<Int>> = tiltDirection
+    fun isAppInitialized(): LiveData<Event<Boolean>> = appInitialized
+    fun init() {
+        appInitialized.value = Event(true)
+        score.value = 0
+    }
 
-     fun generateDirection(){
-         var dir =(1..4).random()
-         tiltDirection.value = Event(dir)
-     }
- }
+    fun getArrowDirection(): LiveData<Event<Int>> = tiltDirection
+    fun getIntervalTime(): LiveData<Event<Long>> = intervalTime
+    //fun getScore(): LiveData<Event<Int>> = score
+
+    fun generateDirection() {
+        var dir = (1..4).random()
+        tiltDirection.value = Event(dir)
+    }
+
+    fun generateInterval() {
+        var interval = (2..5).random()
+        intervalTime.value = Event((interval * 1000).toLong())
+    }
+
+    fun detectedTilt(orientation: Int) {
+        if (waitingToShowArrow) {
+            (score.value.toString().toInt() - 1)
+            return
+        } else if (isTilted) {
+            return
+        } else if (arrowShown) {
+            when (orientation) {
+                in 1..120 -> {// up
+                    if (tiltDirection.value == Event(1)) {
+                        score.value = (score.value.toString().toInt() + 1).toInt()
+                    }
+                }
+
+                in 320..360 -> {//right
+                    if (tiltDirection.value == Event(2)) {
+                        score.value = (score.value.toString().toInt() + 1).toInt()
+                    }
+                }
+                in 240..290 -> {//Down
+                    if (tiltDirection.value == Event(3)) {
+                        score.value = (score.value.toString().toInt() + 1).toInt()
+                    }
+                }
+                in 150..200 -> {//left
+                    if (tiltDirection.value == Event(4)) {
+                        score.value = (score.value.toString().toInt() + 1)
+                    }
+
+                }
+            }
+        }
+
+    }
+    fun getColor() :Int{
+        if(choosenColor.value==0){
+
+        }
+        when(choosenColor.value) {
+            0 -> return R.color.Red
+            1 -> return R.color.Green
+            2 -> return R.color.Pink
+            3 -> return R.color.Blue
+            else -> return R.color.Red
+        }
+    }
+}
